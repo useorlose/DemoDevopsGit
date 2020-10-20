cf563e68684bd6113a5208f433e8d31499324d13
Jyothsna <jyothsnakjntuh@gmail.com>
2020-10-20 15:21:26 +0530

    secondcommit

--- GitDemo/FirstNetCoreApp/Program.cs
+++ GitDemo/FirstNetCoreApp/Program.cs
@@ -7,14 +7,7 @@ namespace FirstNetCoreApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(DateTime.Now.ToString());
-            MathOpDelegate delAdd = Add;
-            int result = delAdd(55, 32);
-            Console.WriteLine("Result of Add:\t" + result.ToString());
-            var arrInts = new int[4] {1,2,3,11}
-            foreach(var x in arrInts){
-            Console.WriteLine(x.ToString());
-                
+            Console.WriteLine( "hi everyone"  );
             }
 
 
